0f3cd008af08acf92853bb99066f41c7fe2fa2f0
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2022-12-13 16:40:21 -0500

    commit tree 4399274faab8ea4ee276564705e88a0bc8abac1b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
 
+    attr_accessor :author_name, :title, :page_count, :genre
 
+    def initialize(author_name, title, page_count, genre)
+        @author_name = author_name
+        @title = title
+        @page_count = page_count
+        @genre = genre
+
+    end
+
+    def turn_page
+    puts "Turning page"
+    end
+
+end
+book = Book.new(authorname, "And Then There Were None", 2200, mystery)
+book.turn_page
